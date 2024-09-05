c95a37030d850b43060e4a5d39be85037bbb809f
ShmuelRozental <rozentalshmuel@gmail.com>
2024-09-05 14:02:36 +0300

    add interface to load json for defenses and threats

--- Program.cs
+++ Program.cs
@@ -13,34 +13,51 @@ namespace Week_10
         {
             //1:
             DefenseStrategiesBST bst = new DefenseStrategiesBST();
-            var defenseStrategies = LoadDefenseStrategies("C:\\Users\\rozen\\source\\repos\\Week-10\\defenceStrategiesBalanced.json");
+            var defenseStrategies = LoadFromJson<DefenseStrategy>("C:\\Users\\rozen\\source\\repos\\Week-10\\defenceStrategiesBalanced.json");
 
             foreach (var strategy in defenseStrategies)
             {
                 bst.Insert(strategy.MinSeverity, strategy.MaxSeverity, strategy.Defenses);
             }
-
+            //2:
             bst.PreOrderTraversal(bst.Root);
+
+            //3:
+            var Threats = LoadFromJson<Threat>("C:\\Users\\rozen\\source\\repos\\Week-10\\threats.json")
+
+
 
 
             Console.ReadLine();
 
         }
 
-        
 
-        public static List<DefenseStrategy> LoadDefenseStrategies(string filePath)
+        public interface IJsonLoadable
         {
-            var jsonString = File.ReadAllText(filePath);
-            var defenseStrategies = JsonSerializer.Deserialize<List<DefenseStrategy>>(jsonString);
-            return defenseStrategies;
+            
         }
 
-        public class DefenseStrategy
+
+        public static List<T> LoadFromJson<T>(string filePath) where T : IJsonLoadable
+        {
+            var jsonString = File.ReadAllText(filePath);
+            var objects = JsonSerializer.Deserialize<List<T>>(jsonString);
+            return objects;
+        }
+
+
+        public class DefenseStrategy: IJsonLoadable
         {
             public int MinSeverity { get; set; }
             public int MaxSeverity { get; set; }
             public List<string> Defenses { get; set; }
+        }
+
+        public class Threat : IJsonLoadable
+        {
+            public int ThreatLevel { get; set; }
+            public string Description { get; set; }
         }
     }
 }
