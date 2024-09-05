07b9f8261d1f84f36624cf0ceef67f09b881457b
ShmuelRozental <rozentalshmuel@gmail.com>
2024-09-05 12:27:48 +0300

    Add project files.

---
+++ BSTNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    internal class BSTNode
+    {
+        public int MaxSeverity { get; set; }
+        public int MinSeverity { get; set; }
+        List<string> Defences { get; set; }
+        public BSTNode Left { get; set; }
+        public BSTNode Right { get; set; }
+        public BSTNode(int minSeverity, int maxSeverity, List<string> defenses) 
+        {
+            MinSeverity = minSeverity;
+            MaxSeverity = maxSeverity;
+            Defences = defenses;
+        }
+    }
+}
